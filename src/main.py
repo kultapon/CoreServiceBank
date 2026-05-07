4f9b166c1259ebf673419ab128e55ab99b7ce615
kultapon <kultsecond@gmail.com>
2026-05-07 22:06:39 +0300

    added schemas, repositories, new router

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import uvicorn
 from fastapi import FastAPI
-
+from src.api.auth import auth_router
 app = FastAPI(title="CoreService")
+
+app.include_router(auth_router)
 
 if __name__ == "__main__":
     uvicorn.run("src.main:app", port=8000, reload=True)
