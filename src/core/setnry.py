bee9e0892dbb8f035073e80534563027d5fbd058
kultapon <kultsecond@gmail.com>
2026-05-08 15:31:35 +0300

    setnry setup

---
+++ src/core/setnry.py
@@ -0,0 +1,33 @@
+import sentry_sdk
+from sentry_sdk.integrations.fastapi import FastApiIntegration
+from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
+
+from src.schemas.config import log_settings
+
+def before_send(event, hint):
+    request = event.get("request")
+
+    if request and "headers" in request:
+        request["headers"].pop("authorization", None)
+
+    return event
+
+def setup_sentry() -> None:
+    if not log_settings.SENTRY_DSN:
+        return
+
+    sentry_sdk.init(
+        dsn=log_settings.SENTRY_DSN,
+
+        integrations=[
+            FastApiIntegration(),
+            SqlalchemyIntegration(),
+        ],
+        before_send=before_send,
+        traces_sample_rate=log_settings.SENTRY_TRACES_SAMPLE_RATE,
+
+        profile_session_sample_rate=1.0,
+        profile_lifecycle="trace",
+
+        environment=log_settings.ENVIRONMENT,
+    )
--- src/main.py
+++ src/main.py
@@ -2,10 +2,15 @@ import uvicorn
 from fastapi import FastAPI
 from src.api.auth import auth_router
 from src.core.errors.handlers import register_exception_handlers
+from src.core.logger import setup_logging
+from src.core.setnry import setup_sentry
 
 app = FastAPI(title="CoreService")
 
 app.include_router(auth_router)
+
+setup_logging()
+setup_sentry()
 
 register_exception_handlers(app)
 
