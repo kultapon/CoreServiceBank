1ea20b9520ee48cc2e173f47fc81f1a8d085555d
kultapon <kultsecond@gmail.com>
2026-05-08 17:27:52 +0300

    logging refactor

--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -1,3 +1,4 @@
+import structlog
 from pwdlib import PasswordHash
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -6,8 +7,10 @@ from src.schemas.config import db_settings
 from src.models import User
 from src.repositories.repositories import UserRepository, RoleRepository
 from src.schemas.user import UserAuth
+logger = structlog.get_logger(__name__)
 
 pwd_hasher = PasswordHash.recommended()
+
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_hasher.verify(plain_password, hashed_password)
