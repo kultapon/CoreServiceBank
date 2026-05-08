2fccfd1800023d5243d9badb058820c7fec12167
kultapon <kultsecond@gmail.com>
2026-05-08 20:05:55 +0300

    added pagination for products list based on user

--- src/repositories/repositories.py
+++ src/repositories/repositories.py
@@ -1,6 +1,6 @@
 
 from sqlalchemy import select
-from sqlalchemy.orm import selectinload
+from sqlalchemy.orm import selectinload, joinedload
 
 from src.models import Role, User
 from src.repositories.base_repository import BaseRepository
@@ -14,31 +14,27 @@ class UserRepository(BaseRepository[User]):
         return result is not None
 
     async def get_user_by_username(self, username: str) -> User | None:
-        result = await self.session.scalars(
-            select(User).where(User.username == username)
-        )
-        return result.one_or_none()
+        return await self.session.scalar(
+            select(User).where(User.username == username))
 
     async def get_user_by_id(self, user_id: int) -> User | None:
         return await self.session.scalar(
             select(User).where(User.id == user_id)
         )
 
-    async def get_user_with_roles(self, user_id: int) -> User | None:
+    async def get_active_user_with_role(
+        self,
+        user_id: int,
+    ) -> User | None:
 
-        return await self.session.scalar(
+        query = (
             select(User)
-            .options(selectinload(User.role))
+            .options(joinedload(User.role))
             .where(User.id == user_id)
+            .where(User.banned_at.is_(None))
         )
 
-    async def get_users(self, user_ids: list[int]):
-        result = await self.session.scalars(
-            select(User)
-            .options(selectinload(User.role))
-            .where(User.id.in_(user_ids))
-        )
-        return result.all()
+        return await self.session.scalar(query)
 
 class RoleRepository(BaseRepository[Role]):
 
--- src/schemas/roles.py
+++ src/schemas/roles.py
@@ -2,5 +2,5 @@ from enum import Enum
 
 class RoleEnum(str, Enum):
     USER = "user"
-    ADVANCED = "moderator"
+    MODERATOR = "moderator"
     ADMIN = "admin"
