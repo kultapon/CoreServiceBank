6dfd024641c6725860378903a9e67ff5bb6d2c53
kultapon <kultsecond@gmail.com>
2026-05-17 17:40:24 +0300

    fix repository and other things

--- src/database.py
+++ src/database.py
@@ -1,6 +1,7 @@
 from typing import Annotated, AsyncGenerator
 
 from fastapi import Depends
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     async_sessionmaker,
@@ -8,6 +9,7 @@ from sqlalchemy.ext.asyncio import (
 )
 from sqlalchemy.orm import DeclarativeBase
 
+from src.core.errors.errors import UniqueConstraintError, DatabaseError
 from src.schemas.config import db_settings
 
 engine = create_async_engine(db_settings.DATABASE_URL, echo=False)
@@ -20,7 +22,31 @@ class Base(DeclarativeBase):
 
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with SessionLocal() as session:
-        yield session
+        try:
+            yield session
+            await session.commit()
+
+        except IntegrityError as e:
+            await session.rollback()
+            msg = str(e.orig).lower()
+            if "duplicate key" in msg or "unique constraint" in msg:
+
+                raise UniqueConstraintError(
+
+                    "Unique constraint violated!"
+
+                ) from e
+
+            else:
+                print(e)
+                raise DatabaseError(f"Database integrity error") from e
+
+        except SQLAlchemyError as e:
+            await session.rollback()
+            raise DatabaseError("Database error") from e
+
+        finally:
+            await session.close()
 
 
 SessionDep = Annotated[AsyncSession, Depends(get_session)]
--- src/repositories/base_repository.py
+++ src/repositories/base_repository.py
@@ -1,10 +1,7 @@
 from typing import Generic, TypeVar
 
 from sqlalchemy import Select
-from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
-
-from src.core.errors.errors import DatabaseError, UniqueConstraintError
 
 T = TypeVar("T")
 
@@ -13,38 +10,20 @@ class BaseRepository(Generic[T]):
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def _commit_with_handling(self) -> None:
-        try:
-            await self.session.commit()
-        except IntegrityError as e:
-            await self.session.rollback()
-            msg = str(e.orig).lower()
-
-            if "duplicate key" in msg or "unique constraint" in msg:
-                raise UniqueConstraintError(
-                    "Unique constraint violated!"
-                ) from e
-            else:
-                print(e)
-                raise DatabaseError(f"Database integrity error") from e
-        except SQLAlchemyError as e:
-            await self.session.rollback()
-            raise DatabaseError("Database error") from e
-
     async def save(self, obj: T) -> T:
         self.session.add(obj)
-        await self._commit_with_handling()
+        await self.session.flush()
         await self.session.refresh(obj)
         return obj
 
     async def delete(self, obj: T) -> None:
         await self.session.delete(obj)
-        await self._commit_with_handling()
 
     async def save_all(self, objs: list[T]) -> list[T]:
         for obj in objs:
             self.session.add(obj)
-        await self._commit_with_handling()
+
+        await self.session.flush()
         return objs
 
     async def perform_custom_query(self, query: Select):
